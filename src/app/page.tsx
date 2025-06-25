0da00f0ed2ea9e8a2c4145de1883593cd1ffdec7
ivanfa434 <ivanfahrudinaziz123@gmail.com>
2025-06-25 09:53:12 +0700

    improve loading in dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
-import { auth } from "@/lib/auth";
-import Image from "next/image";
 import { redirect } from "next/navigation";
 
 export default async function HomePage() {
+  // Preload auth module
+  if (typeof window !== "undefined") {
+    import("@/components/Loading")
+  }
+  
+  const { auth } = await import("@/lib/auth");
   const session = await auth();
-
+  
   if (session) {
     redirect("/dashboard");
   } else {
